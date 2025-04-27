f4ec61c385dad920e2f6d57ed56bbc3d9bb86939
Cooper Olsen <jellymanlvspizza@gmail.com>
2025-04-27 01:13:04 -0400

    Implement seek; add total track count and track length

--- cd.py
+++ cd.py
@@ -1,13 +1,23 @@
 import vlc
+import pycdio
+import cdio
+
 import time
-import os
+import re
 
 class CDPlayer():
     def __init__(self):
+        # vlc
         self.initialise_player()
         self.initialise_events()
+
+        # pycdio
+        self.drive = cdio.Device(source='/dev/sr1', driver_id=pycdio.DRIVER_UNKNOWN)
+
+        self.total_tracks_len = self.get_total_tracks()
+        self.total_tracks_num = len(self.total_tracks_len)
         self.track = 0
-        self.track_length = 0
+        #self.track_length = 0
         self.state = 'stopped'
 
     def initialise_player(self):
@@ -24,8 +34,7 @@ class CDPlayer():
         self.player.event_manager().event_attach(vlc.EventType.MediaPlayerPaused, self.player_paused)
         self.player.event_manager().event_attach(vlc.EventType.MediaPlayerStopped, self.player_stopped)
         self.player.event_manager().event_attach(vlc.EventType.MediaPlayerEndReached, self.player_end_reached)
-        self.listplayer.event_manager().event_attach(vlc.EventType.MediaListPlayerNextItemSet, self.list_player_track_changed)
-        #self.medialist.event_manager().event_attach(vlc.EventType.MediaListEndReached, self.list_end_reached)
+        #self.listplayer.event_manager().event_attach(vlc.EventType.MediaListPlayerNextItemSet, self.list_player_track_changed)
 
     def player_opening(self, event):
         print('Opening...')
@@ -41,17 +50,37 @@ class CDPlayer():
         self.state = 'stopped'
     def player_end_reached(self, event):
         print('End reached')
-        self.track += 1
-    def list_player_track_changed(self, event):
-        print(f'Track: {self.track}')
-        self.track_length = self.get_track_length()
-        print(self.track_length)
-        
-    def get_track_length(self):
-        if self.state == 'playing':
-            time.sleep(1)
-            total = self.get_current_time() / self.get_current_position()
-            return total
+        if self.track < self.total_tracks_num:
+            self.track += 1
+        else:
+            self.stop() # stop playback when end reached
+    #def list_player_track_changed(self, event):
+    #    print(f'Track: {self.track}')
+    #    self.track_length = self.get_track_length()
+    #    print(self.track_length)
+
+    def get_total_tracks(self):
+        track_num = self.drive.get_num_tracks() # get total number of tracks
+        track_msf = []
+        for i in range(1, track_num+2):
+            track_msf.append(self.drive.get_track(i).get_msf()) # get min, sec, frames of each track
+        print(track_msf)
+        track_len = []
+        # because each msf is a string, separate out to calculate integer seconds
+        for i in range(len(track_msf) - 1):
+            len_current = re.split(r':', track_msf[i])
+            len_next = re.split(r':', track_msf[i+1])
+            min = int(len_next[0]) - int(len_current[0])
+            sec = int(len_next[1]) - int(len_current[1])
+            total = (min * 60) + sec # make into seconds
+            track_len.append(total)
+        print(track_len)
+        return track_len
+    #def get_track_length(self):
+    #    if self.state == 'playing':
+    #        time.sleep(1)
+    #        total = self.get_current_time() / self.get_current_position()
+    #        return total
     def get_current_time(self):
         return self.player.get_time() / 1000
     def get_current_position(self):
@@ -67,20 +96,55 @@ class CDPlayer():
             print('start')
             self.play_pause()
         self.listplayer.next()
-        self.track += 1
+        if self.track < self.total_tracks_num:
+            self.track += 1
+        else:
+            self.tstop()
     def prev_track(self):
         if not self.state == 'playing':
             self.play_pause()
-        self.listplayer.previous()
-        self.track -= 1
+        self.listplayer.previous() 
+        if self.track > 0:
+            self.track -= 1
+        else:
+            self.track = self.total_tracks_num
+    def seek_pressed(self, direction):
+        if self.track != 0: # no seeking within opening track
+            if direction == 'forward':
+                k = 1 # forward
+            else:
+                k = -1 # rewind
+            new_pos = self.get_current_position() 
+            if new_pos < 0.99 and new_pos > 0.01: # stop seek when start or end of track reached
+                new_pos += (k * 0.002 / self.total_tracks_len[self.track - 1]) # calculate time delta proportional to track length
+                self.player.set_position(new_pos)
     def stop(self):
         self.listplayer.stop()
+        self.track = 0 # reset track count when stop playing
+    """
+    KEEP! But move to controller class?
+    def eject(self):
+        self.stop()
+        while self.state != 'stopped':
+            print(self.state)
+            time.sleep(0.25)
+        self.drive.eject_media()
+    """
 
 cd = CDPlayer()
 cd.play_pause()
+time.sleep(20)
+print(cd.get_current_position())
+start = time.perf_counter()
+while time.perf_counter() - start < 5:
+    cd.seek_pressed('forward')
+print(cd.get_current_position())
+time.sleep(10)
+"""
 while True:
     print(f'Track {cd.track}: {cd.get_current_time()} / {cd.track_length} ({cd.get_current_position() * 100}%)', end='\r')
     time.sleep(1)
+"""
 """
 while True:
     print(cd.player.get_state())
@@ -92,4 +156,7 @@ while True:
         cd.prev_track()
     elif input() == 's':
         cd.stop()
+    elif input() == 'e':
+        cd.eject()
+    time.sleep(0.5)
 """
